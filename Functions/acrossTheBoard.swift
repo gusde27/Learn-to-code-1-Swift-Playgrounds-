01019170cb1139db0772f9bbfb731b5df816c65f
gusde27 <gusdechang24@gmail.com>
2022-03-06 13:31:00 +0800

    add new 2 file task for this day

---
+++ Functions/acrossTheBoard.swift
@@ -0,0 +1,18 @@
+func nice() {
+    moveForward()
+    collectGem()
+}
+
+collectGem()
+nice()
+nice()
+turnRight()
+nice()
+turnRight()
+nice()
+nice()
+turnLeft()
+nice()
+turnLeft()
+nice()
+nice()
