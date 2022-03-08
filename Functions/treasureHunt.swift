f90f68d306ef122824f9a44a7fe0f6a65be0f47a
gusde27 <gusdechang24@gmail.com>
2022-03-08 14:05:36 +0800

    add treasure hunt

---
+++ Functions/treasureHunt.swift
@@ -0,0 +1,34 @@
+func movefor4times(){
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    }
+
+func smalltoggle(){
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+
+func fartoggle(){
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    movefor4times()
+}
+
+smalltoggle()
+smalltoggle()
+turnLeft()
+fartoggle()
+fartoggle()
