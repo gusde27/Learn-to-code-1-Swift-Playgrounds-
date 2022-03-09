a425d65c4d101ac72a521b71386dea4cb8cfdd55
gusde27 <gusdechang24@gmail.com>
2022-03-10 00:07:06 +0800

    something new on for loops

---
+++ For Loops/fourStashSweep.swift
@@ -0,0 +1,24 @@
+func turnaround(){
+    turnLeft()
+    turnLeft()
+}
+
+func stage() {
+    for o in 1 ... 2 {
+    moveForward()
+    collectGem()
+    turnLeft()
+    moveForward()
+    collectGem()
+    turnaround()
+    }
+}
+
+for i in 1 ... 4 {
+    stage()
+    moveForward()
+    turnLeft()
+    moveForward()
+    collectGem()
+    moveForward()
+}
